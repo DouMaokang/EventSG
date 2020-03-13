841447109793f0a6d4e07e050fccfccd43d2db2f
Chen Xinyi <xchen032@e.ntu.edu.sg>
2020-03-13 19:37:07 +0800

    Integrated navigation bar in main

--- event_sg/lib/homepage.dart
+++ event_sg/lib/homepage.dart
@@ -2,17 +2,10 @@ import 'package:event_sg/components/event_list.dart';
 import 'package:event_sg/components/event_list_item.dart';
 import 'package:flutter/material.dart';
 
+import 'event_details.dart';
+
 class Homepage extends StatefulWidget {
   Homepage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
   List<EventListItem> recommendedEventList = [];
@@ -32,16 +25,9 @@ class _HomepageState extends State<Homepage> {
       widget.recommendedEventList.add(EventListItem(
         eventVenue: "NTU", eventDate: "May 1", eventName: "Event1",));
     }
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
           title: Text("Homepage"),
           actions: <Widget>[
             // action button
@@ -54,32 +40,11 @@ class _HomepageState extends State<Homepage> {
           ]
       ),
 
-      body: Center(child: buildBody()),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Home'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_box),
-            title: Text('Post'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.notifications),
-            title: Text('Notification'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            title: Text('Profile'),
-          ),
-        ],
-        type: BottomNavigationBarType.fixed,
-      ),
+      body: _buildBody(),
     );
   }
 
-  Widget buildBody() {
+  _buildBody() {
     return new ListView(
                   scrollDirection: Axis.vertical,
                   shrinkWrap: true,
--- event_sg/lib/main.dart
+++ event_sg/lib/main.dart
@@ -1,37 +1,84 @@
-import 'package:event_sg/homepage.dart';
 import 'package:flutter/material.dart';
 // We can import files from its package.
 import 'event_details.dart';
-
-
+import 'homepage.dart';
 
 void main() => runApp(MaterialApp(
-  initialRoute: '/homepage', // '/' is the default initialRoute.
   routes: {
-    '/event_details': (context) => EventDetails(), // This is the base route.
-    '/homepage': (context) => Homepage(),
+    '/': (context) => MyBottomNavBar(), // This is the base route.
+    '/event_details': (context) => EventDetails(),
   },
 
 ));
 
+
+/// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "EventSG",
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: Homepage(title: "Home"),
+      title: _title,
+      home: MyBottomNavBar(),
+    );
+  }
+}
+
+class MyBottomNavBar extends StatefulWidget {
+  @override
+  _MyBottomNavBarState createState() => _MyBottomNavBarState();
+}
+
+class _MyBottomNavBarState extends State<MyBottomNavBar> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgetOptions = <Widget>[
+    Homepage(),
+    Text("2"),
+    Text("3"),
+    Text("4"),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar:
+        BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
+
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_box),
+              title: Text('Post'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.notifications),
+              title: Text('Notification'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.amber[800],
+          onTap: _onItemTapped,
+          type: BottomNavigationBarType.fixed, // We need to add this line when having > 3 icons.
+        )
     );
   }
 }
