fb32f170e88daf91aa6ec3c2526b9d65ca3fb25d
Shivani <shivani001@e.ntu.edu.sg>
2020-03-29 15:10:51 +0800

    added events saved (still needs a bit of modification) and set up firebase

--- event_sg/lib/presentation/components/event_list.dart
+++ event_sg/lib/presentation/components/event_list.dart
@@ -8,6 +8,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
 
+import '../../models/event.dart';
+import '../../models/event.dart';
+
 
 class EventListItem extends StatelessWidget {
 
@@ -119,16 +122,9 @@ class EventListItem extends StatelessWidget {
                               ),
                               Expanded(
                                 flex: 3,
-                                child: IconButton(
-                                  alignment: Alignment.centerRight,
-                                  padding: EdgeInsets.fromLTRB(0, 0, 12, 0),
-                                  iconSize: 20,
-                                  icon: Icon(
-                                    Icons.favorite_border,
-                                  ),
-                                  onPressed: () {},
-                                ),
+                                  child:_buildFavoriteButton(context),
                               )
+
                             ],
                           ),
                         ),
@@ -137,6 +133,33 @@ class EventListItem extends StatelessWidget {
                 ),
               ],),
           ),
+        );
+      },
+
+    );
+
+  }
+
+  Widget _buildFavoriteButton(BuildContext context) {
+    final bloc = BlocProvider.of<EventSavedBloc>(context);
+    return StreamBuilder<List<Event>>(
+      stream: bloc.favoritesStream,
+      initialData: bloc.favorites,
+      builder: (context, snapshot) {
+        List<Event> favorites =
+        (snapshot.connectionState == ConnectionState.waiting)
+            ? bloc.favorites
+            : snapshot.data;
+        bool isFavorite = favorites.contains(event);
+
+        return FlatButton.icon(
+          // 2
+          onPressed: () => bloc.toggleRestaurant(event),
+          textColor: isFavorite ? Theme
+              .of(context)
+              .accentColor : null,
+          icon: Icon(isFavorite ? Icons.favorite : Icons.favorite_border),
+          label: Text('Favorite'),
         );
       },
     );
