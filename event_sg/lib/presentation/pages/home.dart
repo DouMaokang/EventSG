960ee6dd4c2a58a8645f2cca5402c694b326f495
DouMaokang <alecdou0001@gmail.com>
2020-03-31 05:32:03 +0800

    added load more button

--- event_sg/lib/presentation/pages/home.dart
+++ event_sg/lib/presentation/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:event_sg/api_clients/event_api_client.dart';
 import 'package:event_sg/blocs/blocs.dart';
 import 'package:event_sg/presentation/components/components.dart';
+import 'package:event_sg/presentation/components/loadable_events.dart';
 import 'package:event_sg/presentation/sub_pages/sub_pages.dart';
 import 'package:event_sg/repositories/repositories.dart';
 import 'package:flutter/material.dart';
@@ -51,7 +52,7 @@ class Homepage extends StatelessWidget {
 
 
           Container(
-            decoration: BoxDecoration(color: Colors.blue[50]),
+            decoration: BoxDecoration(color: Colors.pink[50]),
             child: ListTile(
               contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
 
@@ -207,6 +208,79 @@ class Homepage extends StatelessWidget {
               }
             },
           ),
+
+          Divider(),
+          Container(
+            decoration: BoxDecoration(color: Colors.cyan[50]),
+            child: ListTile(
+              contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+
+              title: Padding(
+                padding: EdgeInsets.symmetric(vertical: 8),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      "All Events",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20
+                      ),
+                    ),
+                    SizedBox(width: 6,),
+                    Icon(Icons.border_all, color: Colors.black, size: 20,),
+                  ],
+                ),
+              ),
+
+              subtitle: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(
+                      "Here is a full list of events",
+                      style: TextStyle(color: Colors.black),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                      softWrap: true,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 6,),
+
+          BlocBuilder<EventListBloc, EventListState>(
+            // ignore: missing_return
+            builder: (context, state) {
+              if (state is EventListEmpty) {
+                return Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Events not loaded!',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                );
+              } else if (state is EventListLoading) {
+                return Center(child: CircularProgressIndicator());
+              } else if (state is EventListLoaded) {
+                return LoadableEvents(eventList: state.allEvents,);
+              } else {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Text(
+                      'Something went wrong!',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                );
+              }
+            },
+          ),
+
         ],
       ),
       backgroundColor: Colors.white,
