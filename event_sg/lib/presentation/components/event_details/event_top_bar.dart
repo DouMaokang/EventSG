d268d945f3d1abda9e8ad45abbd56e9d918e5098
ewigkeit <15670318223@163.com>
2020-03-30 17:37:48 +0800

    added ReviewAddingPage

--- event_sg/lib/presentation/components/event_details/event_top_bar.dart
+++ event_sg/lib/presentation/components/event_details/event_top_bar.dart
@@ -2,6 +2,8 @@ import 'package:event_sg/api_clients/api_clients.dart';
 import 'package:event_sg/blocs/blocs.dart';
 import 'package:event_sg/blocs/event_saved_bloc.dart';
 import 'package:event_sg/blocs/event_saved_state.dart';
+import 'package:event_sg/models/models.dart';
+import 'package:event_sg/presentation/sub_pages/add_review.dart';
 import 'package:event_sg/repositories/repositories.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -33,21 +35,25 @@ class _EventTopBarState extends State<EventTopBar> {
     eventApiClient: EventApiClient(httpClient: http.Client()),
   );
 
+  final ReviewRepository reviewRepository = ReviewRepository(
+    reviewApiClient: ReviewApiClient(httpClient: http.Client())
+  );
+
 
   bool _hasSaved = false;
+  bool _hasReviewed = false;
 
   @override
   void initState() {
     _checkHasSaved(eventId: widget.eventId, userId: widget.userId);
+    _checkHasReviewed(eventId: widget.eventId, userId: widget.userId);
     super.initState();
   }
 
 
   @override
   Widget build(BuildContext context) {
-
-
-
+    _checkHasReviewed(eventId: widget.eventId, userId: widget.userId);
     return PreferredSize(
       preferredSize: Size.fromHeight(kToolbarHeight),
       child: AppBar(
@@ -65,6 +71,22 @@ class _EventTopBarState extends State<EventTopBar> {
             child: const Text('Event Details')
         ),
         actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add_circle_outline),
+            onPressed: () {
+              _checkHasReviewed(eventId: widget.eventId, userId: widget.userId);
+              if (_hasReviewed) {
+                Toast.show("You have already added a review", context, duration: Toast.LENGTH_SHORT, gravity:  Toast.TOP); // remove if you want
+              } else {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context)=>ReviewAddingPage(eventId: widget.eventId)),
+                );
+              }
+            },
+          ),
+
+          // -------------------------- save event button- ---------------------
           BlocProvider<EventSavedBloc>(
             create: (context) => EventSavedBloc(eventRepository: eventRepository),
 
@@ -105,6 +127,13 @@ class _EventTopBarState extends State<EventTopBar> {
     bool result = await eventRepository.hasSavedEvent(eventId: eventId, userId: userId);
     setState(() {
       _hasSaved = result;
+    });
+  }
+
+  _checkHasReviewed({String eventId, String userId}) async {
+    bool result = await reviewRepository.hasReviewed(eventId: eventId, userId: userId);
+    setState(() {
+      _hasReviewed = result;
     });
   }
 }
--- event_sg/lib/repositories/repositories.dart
+++ event_sg/lib/repositories/repositories.dart
@@ -1,1 +1,2 @@
 export 'event_repository.dart';
+export 'review_repository.dart';
