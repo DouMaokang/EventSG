e6fcfe454bb5ab5cdeec142a6034f123e411cab1
ewigkeit <15670318223@163.com>
2020-03-31 15:01:25 +0800

    added user avatar and username to feedback page

--- event_sg/lib/presentation/components/event_feedback/feedback_header.dart
+++ event_sg/lib/presentation/components/event_feedback/feedback_header.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:event_sg/models/models.dart';
 
 class FeedbackHeader extends StatelessWidget {
-  final String url;
-  final String eventName;
+  final Event event;
 
-  FeedbackHeader({this.url, @required this.eventName});
+  FeedbackHeader({@required this.event});
 
   @override
   Widget build(BuildContext context) {
@@ -12,41 +12,31 @@ class FeedbackHeader extends StatelessWidget {
         children: <Widget>[
           // child 1 head image ----------------------------------------------------------------
           DecoratedBox(
-            decoration: BoxDecoration(color: Colors.grey[200]),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                  horizontal: 16.0, vertical: 8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  boxShadow: [BoxShadow(
-                    color: Colors.grey[400],
-                    blurRadius: 10.0, // has the effect of softening the shadow
-                    spreadRadius: 5.0, // has the effect of extending the shadow
-                    offset: Offset(
-                      5.0, // horizontal, move right 10
-                      2.0, // vertical, move down 10
+              decoration: BoxDecoration(color: Colors.white),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 0.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(event.image),
+                      fit: BoxFit.cover,
                     ),
-                  )
-                  ],
-                  image: DecorationImage(
-                      image: NetworkImage(this.url),
-                      fit: BoxFit.cover
                   ),
+                  height: 200,
                 ),
-                height: 200,
-              ),
-            ),
+              )
           ),
           SizedBox(height: 16,),
           // child 2 --event headline-------------------------------------------
           Padding(
-            padding: const EdgeInsets.only(bottom: 0, left: 12, top: 12),
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
             child: Container(
               width: double.infinity,
               child: Text(
-                this.eventName,
+                event.title,
                 textAlign: TextAlign.left,
                 style: TextStyle(
+
                     fontSize: 38,
                     fontWeight: FontWeight.bold
                 ),
